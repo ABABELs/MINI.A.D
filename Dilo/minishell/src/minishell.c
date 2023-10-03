93ffea8ea672f60ae33c6961bf9e485fd12dbca7
Dilovan Candan <dilovancandan@MacBook-Pro-de-Dilovan.local>
2023-10-03 16:09:06 +0200

    no_pipe split en deux

--- Dilo/minishell/src/minishell.c
+++ Dilo/minishell/src/minishell.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aabel <aabel@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dilovancandan <dilovancandan@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 18:32:05 by dilovancand       #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/10/03 15:32:39 by dilovancand      ###   ########.fr       */
-=======
-/*   Updated: 2023/10/03 15:38:58 by aabel            ###   ########.fr       */
->>>>>>> 5eb635aedaae487250d1694126112216b9bfe779
+/*   Updated: 2023/10/03 16:08:33 by dilovancand      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,23 +58,33 @@ static void	remove_quotes(t_mantle *mantle)
 	}
 }
 
-static void	no_pipe(const char *str, char **env)
+static t_crust	*malloc_crust(const char *str, char **env)
 {
-	t_crust		*crust;
-	char		**tab;
 	char		*get_env;
+	t_crust		*crust;
 
 	crust = malloc(sizeof(t_crust));
 	crust->lst_cmd = malloc(sizeof(t_mantle));
 	crust->lst_cmd->first = NULL;
 	if (!crust || !crust->lst_cmd)
-		return ;
+		return (NULL);
 	crust->input = (char *)str;
 	crust->syntax_error = 0;
 	crust->env = array_dup(env);
 	get_env = ft_getenv(crust, ft_strdup("PATH"));
 	crust->path = ft_split(get_env, ':');
 	free(get_env);
+	return (crust);
+}
+
+static void	no_pipe(const char *str, char **env)
+{
+	t_crust		*crust;
+	char		**tab;
+
+	crust = malloc_crust(str, env);
+	if (!crust)
+		return ;
 	tab = ft_minisplit(crust->input);
 	crust->root_path = ft_getenv(crust, ft_strdup("HOME"));
 	if (!tab || !tab[0])
@@ -89,7 +95,8 @@ static void	no_pipe(const char *str, char **env)
 	ft_type_set(crust->lst_cmd);
 	if (ft_after_redir(crust->lst_cmd) == -1)
 		return ;
-	(ft_heredoc(crust->lst_cmd), env_var_expension(crust, crust->lst_cmd->first));
+	ft_heredoc(crust->lst_cmd);
+	env_var_expension(crust, crust->lst_cmd->first);
 	(remove_quotes(crust->lst_cmd), ft_joincmd(crust->lst_cmd));
 	(ft_open_fd(crust->lst_cmd), join_the_pipe(crust));
 	(pipe_or_not(crust), last_exit_code(crust->lst_cmd->first));
